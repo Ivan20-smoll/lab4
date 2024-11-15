d7e68ec7d289aa1530e924be63e76370ca147592
Ivan20-smoll <orlovskijivan3@gmail.com>
2024-11-15 14:56:45 +0300

    Update Найти сумму произведений из списка словарей.py

--- Найти сумму произведений из списка словарей.py
+++ Найти сумму произведений из списка словарей.py
@@ -1,6 +1,22 @@
 # TODO решите задачу
-def task() -> float:
-    ...
+import json
 
+def task() -> float:
+    """
+    Функция вычисляет сумму произведений значений "score" и "weight"
+    в каждом словаре JSON файла.
+
+    Returns:
+        float: Сумма произведений, округлённая до 3 знаков после запятой.
+    """
+
+    with open('task.json', 'r') as f:
+        data = json.load(f)
+
+    total_sum = 0
+    for item in data:
+        total_sum += item['score'] * item['weight']
+
+    return round(total_sum, 3)
 
 print(task())
